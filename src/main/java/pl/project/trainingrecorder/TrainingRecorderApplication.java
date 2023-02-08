852d05ead41e6f3a8eb39a902d092a25f6893863
cyboranf <cyboranf@gmail.com>
2023-02-08 22:27:05 +0100

    project -created

---
+++ src/main/java/pl/project/trainingrecorder/TrainingRecorderApplication.java
@@ -0,0 +1,13 @@
+package pl.project.trainingrecorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingRecorderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrainingRecorderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/project/trainingrecorder/CaRentalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.project.trainingrecorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaRentalProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
