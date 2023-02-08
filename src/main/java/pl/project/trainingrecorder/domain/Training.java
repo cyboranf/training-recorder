7e3afc191faf664612314d93ba738a1c4ee8ca16
cyboranf <cyboranf@gmail.com>
2023-02-08 22:51:12 +0100

    training entity -added

---
+++ src/main/java/pl/project/trainingrecorder/domain/Training.java
@@ -0,0 +1,36 @@
+package pl.project.trainingrecorder.domain;
+
+import lombok.Data;
+import lombok.Generated;
+import org.intellij.lang.annotations.Pattern;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.lang.Nullable;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Table(name = "training")
+@Entity
+@Data
+public class Training {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "training_day")
+    @DateTimeFormat(pattern = "dd-mm-yyyy")
+    private LocalDate trainingDay;
+
+    @Column(name = "km")
+    private int kilometers;
+    @Column(name = "training_time")
+    @DateTimeFormat(pattern = "hh:mm:ss")
+    private LocalTime trainingTime;
+    @Column(name = "kcal")
+    private long kCal;
+    @Column(name = "description")
+    @Nullable
+    private String description;
+
+}
