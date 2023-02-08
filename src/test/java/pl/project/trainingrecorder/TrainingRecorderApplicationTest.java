4c1d67091fed51caead580872746faf05af3e9a5
cyboranf <cyboranf@gmail.com>
2023-02-08 22:29:41 +0100

    test -rename

--- src/test/java/pl/project/trainingrecorder/TrainingRecorderApplicationTest.java
+++ src/test/java/pl/project/trainingrecorder/TrainingRecorderApplicationTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CaRentalProjectApplicationTests {
+class TrainingRecorderApplicationTest {
 
     @Test
     void contextLoads() {
