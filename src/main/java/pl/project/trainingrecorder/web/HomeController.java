bfb5a0d797b600ec5c21f8a04e20240c63caab62
cyboranf <cyboranf@gmail.com>
2023-02-08 22:28:44 +0100

    method to show landing page -done

---
+++ src/main/java/pl/project/trainingrecorder/web/HomeController.java
@@ -0,0 +1,12 @@
+package pl.project.trainingrecorder.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String showHome() {
+        return "home";
+    }
+}
