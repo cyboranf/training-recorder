0fdc792bde2ad715d912fab83e6bf94bf19fe8a2
cyboranf <cyboranf@gmail.com>
2023-02-09 23:37:57 +0100

    find user by username method -added

--- src/main/java/pl/project/trainingrecorder/repository/UserRepository.java
+++ src/main/java/pl/project/trainingrecorder/repository/UserRepository.java
@@ -3,5 +3,8 @@ package pl.project.trainingrecorder.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import pl.project.trainingrecorder.domain.User;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUserName(String userName);
 }
