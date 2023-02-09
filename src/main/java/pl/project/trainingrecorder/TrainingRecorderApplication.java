00fc2e6f153938fd3f535e0da9fa74aec997c8fc
cyboranf <cyboranf@gmail.com>
2023-02-09 23:36:44 +0100

    basePackages -changed

--- src/main/java/pl/project/trainingrecorder/TrainingRecorderApplication.java
+++ src/main/java/pl/project/trainingrecorder/TrainingRecorderApplication.java
@@ -2,8 +2,11 @@ package pl.project.trainingrecorder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import pl.project.trainingrecorder.repository.UserRepository;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackageClasses = UserRepository.class)
 public class TrainingRecorderApplication {
 
     public static void main(String[] args) {
