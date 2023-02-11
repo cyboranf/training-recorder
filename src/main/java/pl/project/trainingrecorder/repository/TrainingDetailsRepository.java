e17134a5da0ea288a5a770406a3757a4cab06ded
cyboranf <cyboranf@gmail.com>
2023-02-11 20:23:47 +0100

    new methods to JPA -added

--- src/main/java/pl/project/trainingrecorder/repository/TrainingDetailsRepository.java
+++ src/main/java/pl/project/trainingrecorder/repository/TrainingDetailsRepository.java
@@ -1,7 +1,11 @@
 package pl.project.trainingrecorder.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import pl.project.trainingrecorder.domain.Training;
 import pl.project.trainingrecorder.domain.TrainingDetails;
 
+import java.util.Optional;
+
 public interface TrainingDetailsRepository extends JpaRepository<TrainingDetails, Long> {
+    Optional<TrainingDetails> findById(long id);
 }
--- src/main/java/pl/project/trainingrecorder/repository/TrainingRepository.java
+++ src/main/java/pl/project/trainingrecorder/repository/TrainingRepository.java
@@ -2,6 +2,12 @@ package pl.project.trainingrecorder.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import pl.project.trainingrecorder.domain.Training;
+import pl.project.trainingrecorder.domain.User;
+
+import java.util.Optional;
 
 public interface TrainingRepository extends JpaRepository<Training, Long> {
+    Optional<Training> findById(long id);
+
+
 }
