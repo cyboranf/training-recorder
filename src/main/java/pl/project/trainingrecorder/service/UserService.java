ea436fbb4ba3338ec6bc3be86c2c5dadb21d19e6
cyboranf <cyboranf@gmail.com>
2023-02-11 17:45:32 +0100

    implementation to User JPA methods -added

--- src/main/java/pl/project/trainingrecorder/service/UserService.java
+++ src/main/java/pl/project/trainingrecorder/service/UserService.java
@@ -2,8 +2,12 @@ package pl.project.trainingrecorder.service;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import pl.project.trainingrecorder.domain.Training;
 import pl.project.trainingrecorder.domain.User;
 import pl.project.trainingrecorder.repository.UserRepository;
+
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
 
 @Service
 @Transactional
@@ -22,7 +26,22 @@ public class UserService {
         return userRepository.findFirstByEmail(email);
     }
 
-    public User findByLogged(){
+    public User findByLogged() {
         return userRepository.findByLogged().get(0);
+    }
+
+    public int loggedUserTrainingCount() {
+        int count = userRepository.findByLogged().get(0).getTrainingList().size();
+        return count;
+    }
+
+    public long loggedUserBurnedCalories() {
+        AtomicLong kcal = new AtomicLong();
+        List<Training> trainingList = userRepository.findByLogged().get(0).getTrainingList();
+
+        trainingList.forEach(t -> {
+            kcal.addAndGet(t.getTrainingDetails().getKcal());
+        });
+        return kcal.get();
     }
 }
